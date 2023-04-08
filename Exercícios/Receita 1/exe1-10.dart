16b48457e177fdede1528a450f3040b3e3d9bb52
AndersonAzeved <andersonsilva14.2017@gmail.com>
2023-04-08 17:06:25 -0300

    Exercício 8

--- Exercícios/Receita 1/exe1-10.dart
+++ Exercícios/Receita 1/exe1-10.dart
@@ -8,7 +8,7 @@ void main() {
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 223, 178, 180),
+        backgroundColor: const Color.fromARGB(255, 223, 178, 180),
         appBar: AppBar(
           centerTitle: true,
           title: const Text("The Beers"),
@@ -21,10 +21,28 @@ void main() {
                   fontWeight: FontWeight.bold,
                 )),
             const Text(
-                '\nA cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.  Acredita-se que tenha sido uma das primeiras bebidas alcoólicas que foram criadas pelo ser humano. Atualmente, é a terceira bebida mais popular do mundo, logo depois da água e do café.',
+                '\nA cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.  Acredita-se que tenha sido uma das primeiras bebidas alcoólicas que foram criadas pelo ser humano. Atualmente, é a terceira bebida mais popular do mundo, logo depois da água e do café.\n',
                 style: TextStyle(
-                  fontSize: 10.0,
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.bold,
                 )),
+            SizedBox(
+              height: 100.0,
+              width: 50.0,
+              child: Column(
+                children: [
+                  Image.network(
+                      "https://imgs.casasbahia.com.br/55017372/1g.jpg?imwidth=500"),
+                  const Text("Budweiser"),
+                  Image.network(
+                      "https://a-static.mlcdn.com.br/800x560/cerveja-long-neck-coronita-06x210ml-ambev/aguaja/7891149108756/ed815bc1aa0dedf386a337c8653592df.png"),
+                  const Text("Corona"),
+                  Image.network(
+                      "https://embalagemmarca.com.br/wp-content/uploads/2021/08/Spaten.jpeg"),
+                  const Text("Spaten"),
+                ],
+              ),
+            ),
             Expanded(
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
