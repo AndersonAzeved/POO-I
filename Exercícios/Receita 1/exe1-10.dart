ac3a019ac124bce3dd4ae66cdb29380f0322269a
AndersonAzeved <andersonsilva14.2017@gmail.com>
2023-04-09 11:14:50 -0300

    Exercício 9

--- Exercícios/Receita 1/exe1-10.dart
+++ Exercícios/Receita 1/exe1-10.dart
@@ -13,64 +13,114 @@ void main() {
           centerTitle: true,
           title: const Text("The Beers"),
         ),
-        body: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            const Text('\nCatálogo',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                )),
-            const Text(
-                '\nA cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.  Acredita-se que tenha sido uma das primeiras bebidas alcoólicas que foram criadas pelo ser humano. Atualmente, é a terceira bebida mais popular do mundo, logo depois da água e do café.\n',
-                style: TextStyle(
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold,
-                )),
-            SizedBox(
-              height: 100.0,
-              width: 50.0,
-              child: Column(
-                children: [
-                  Image.network(
-                      "https://imgs.casasbahia.com.br/55017372/1g.jpg?imwidth=500"),
-                  const Text("Budweiser"),
-                  Image.network(
-                      "https://a-static.mlcdn.com.br/800x560/cerveja-long-neck-coronita-06x210ml-ambev/aguaja/7891149108756/ed815bc1aa0dedf386a337c8653592df.png"),
-                  const Text("Corona"),
-                  Image.network(
-                      "https://embalagemmarca.com.br/wp-content/uploads/2021/08/Spaten.jpeg"),
-                  const Text("Spaten"),
-                ],
-              ),
-            ),
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+        body: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          const Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  icon: const Icon(
-                      Icons.sports_bar), //icon data for elevated button
-                  label: const Text("Cervejar"), //label text
+                Text('\nCatálogo',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text(
+                    '\nA cerveja é uma bebida produzida a partir da fermentação de cereais, principalmente a cevada maltada.  Acredita-se que tenha sido uma das primeiras bebidas alcoólicas que foram criadas pelo ser humano. Atualmente, é a terceira bebida mais popular do mundo, logo depois da água e do café.\n',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            ),
+          ),
+
+          //Imagens
+          Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 50.0,
+                  width: 120.0,
+                  child: Column(children: [
+                    const ListTile(
+                      title: Text('Budweiser'),
+                      tileColor: Colors.red,
+                    ),
+                    FadeInImage.assetNetwork(
+                        fadeInDuration: const Duration(seconds: 2),
+                        placeholder: 'assets/images/loading.gif',
+                        image:
+                            'https://imgs.casasbahia.com.br/55017372/1g.jpg?imwidth=500'),
+                  ]),
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  icon: const Icon(Icons.info), //icon data for elevated button
-                  label: const Text("Informação"), //label text
+                SizedBox(
+                  height: 50.0,
+                  width: 120.0,
+                  child: Column(children: [
+                    const ListTile(
+                      title: Text('Spaten'),
+                      tileColor: Colors.red,
+                    ),
+                    FadeInImage.assetNetwork(
+                        fadeInDuration: const Duration(seconds: 2),
+                        placeholder: 'assets/images/loading.gif',
+                        image:
+                            'https://imgs.casasbahia.com.br/1554019029/1xg.jpg?imwidth=500'),
+                  ]),
                 ),
-                ElevatedButton.icon(
-                  onPressed: () {},
-                  onLongPress: () {},
-                  icon:
-                      const Icon(Icons.logout), //icon data for elevated button
-                  label: const Text("Sair"), //label text
+                SizedBox(
+                  height: 50.0,
+                  width: 120.0,
+                  child: Column(children: [
+                    const ListTile(
+                      title: Text('Corona'),
+                      tileColor: Colors.red,
+                    ),
+                    FadeInImage.assetNetwork(
+                        fadeInDuration: const Duration(seconds: 2),
+                        placeholder: 'assets/images/loading.gif',
+                        image:
+                            'https://imgs.casasbahia.com.br/1508765371/1xg.jpg'),
+                  ]),
                 ),
               ],
-            )),
-          ]),
-        ),
+            ),
+          ),
+
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    onLongPress: () {},
+                    icon: const Icon(
+                        Icons.sports_bar), //icon data for elevated button
+                    label: const Text("Cervejar"), //label text
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    onLongPress: () {},
+                    icon:
+                        const Icon(Icons.info), //icon data for elevated button
+                    label: const Text("Informação"), //label text
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    onLongPress: () {},
+                    icon: const Icon(
+                        Icons.logout), //icon data for elevated button
+                    label: const Text("Sair"), //label text
+                  ),
+                ],
+              ),
+            ],
+          )),
+        ]),
+
+        //BottomNavigation
         bottomNavigationBar:
             BottomNavigationBar(items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
